3ac097f64691fcbc8e362a45bde64dc49747cb15
Md Hefzul Bari <92200936+RAtul14ApriL@users.noreply.github.com>
2021-11-20 14:38:33 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+let convertBtn = document.getElementById('convert');
+let resetBtn = document.getElementById('reset');
+
+convertBtn.addEventListener('click', convertTemp);
+
+function convertTemp() {
+
+  function convertKelvin() {
+    document.getElementById('kelvinResult').innerHTML = parseInt(document.getElementById('temperature').value) + " degree Kelvin";
+    document.getElementById('celsiusResult').innerHTML = (parseInt(document.getElementById('temperature').value) - 273) + " degree Celsius";
+    document.getElementById('fahrenheitResult').innerHTML = Math.floor(((parseInt(document.getElementById('temperature').value) - 273) * (9 / 5) + 32)) + " degree Fahrenheit";
+  }
+
+  function convertCelsius() {
+    document.getElementById('celsiusResult').innerHTML = parseInt(document.getElementById('temperature').value) + " degree Celsius";
+    document.getElementById('kelvinResult').innerHTML = (parseInt(document.getElementById('temperature').value) + 273) + " degree Kelvin";
+    document.getElementById('fahrenheitResult').innerHTML = Math.floor((parseInt(document.getElementById('temperature').value) * (9 / 5)) + 32) + " degree Fahrenheit";
+  }
+
+  function convertFahrenheit() {
+    document.getElementById('fahrenheitResult').innerHTML = parseInt(document.getElementById('temperature').value) + " degree fahrenheit";
+    document.getElementById('kelvinResult').innerHTML = Math.floor((parseInt(document.getElementById('temperature').value) - 32) * (5 / 9) + 273) + " degree Kelvin";
+    document.getElementById('celsiusResult').innerHTML = Math.floor((parseInt(document.getElementById('temperature').value) - 32) * (5 / 9)) + " degree Celsius";
+  }
+  if (document.getElementById('units').value == 'kelvin') {
+    convertKelvin();
+  } else if (document.getElementById('units').value == 'celsius') {
+    convertCelsius();
+  } else if (document.getElementById('units').value == 'fahrenheit') {
+    convertFahrenheit();
+  } else if (document.getElementById('units').value == 'select') {
+    alert("SELECT UNIT");
+  }
+}
+
+resetBtn.addEventListener('click', resetValue);
+
+function resetValue() {
+  document.getElementById("temperature").value = '';
+  document.getElementById("celsiusResult").innerHTML = '';
+  document.getElementById("kelvinResult").innerHTML = '';
+  document.getElementById("fahrenheitResult").innerHTML = '';
+  document.getElementById('units').value = 'select';
+}
